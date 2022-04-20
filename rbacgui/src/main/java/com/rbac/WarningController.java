9483a33729e5d1c1d0d50983f7b1a62dd127bffe
simoneancona04 <simone.ancona.2004@calvino.edu.it>
2022-04-20 18:46:46 +0200

    warning

--- rbacgui/src/main/java/com/rbac/WarningController.java
+++ rbacgui/src/main/java/com/rbac/WarningController.java
@@ -1,5 +1,46 @@
 package com.rbac;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
+
 public class WarningController {
+
+    private Stage st;
     
+    @FXML
+    Button okButton;
+
+    @FXML
+    Label message;
+
+    @FXML
+    public void initialize(String message) {
+        this.message.setText(message);
+    }
+
+    public WarningController() {}
+
+    public WarningController(String message) {
+        st = new Stage();
+        try {
+            st.setScene(new Scene(App.loadFXML("warning")));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        st.initStyle(StageStyle.UNDECORATED);
+        this.message = new Label(message);
+
+        st.showAndWait();
+    }
+
+    public void close() {
+        st.close();
+    }
+
 }
