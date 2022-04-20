f8845bf1e52073de452221558aae499ccf949cfa
simone <simonechiarlo1@gmail.com>
2022-04-20 22:24:51 +0200

    added import

--- rbacgui/src/main/java/com/rbac/LoginController.java
+++ rbacgui/src/main/java/com/rbac/LoginController.java
@@ -2,11 +2,8 @@ package com.rbac;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class LoginController {
 
