7dcbd60ec06d91ffc9279aa9a2ba7ca996c7a3d3
simone <simonechiarlo1@gmail.com>
2022-04-20 15:41:48 +0200

    added jackson

--- rbacgui/src/main/java/module-info.java
+++ rbacgui/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.rbac {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
 
     opens com.rbac to javafx.fxml;
     exports com.rbac;
